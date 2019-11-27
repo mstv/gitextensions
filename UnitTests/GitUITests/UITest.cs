a58e5a0e4ba7add2f0b770b2a654a3a1de79c886
Michael Seibt <mstv@gmx.net>
2019-11-27 22:07:36 +0100

    Resolve race condition in UITest.RunForm
    - register for Application.Idle always *before* opening the form
    - cleanup everything on exception
    - remove WaitForIdleAsync because of high risk for race condition

--- UnitTests/GitUITests/UITest.cs
+++ UnitTests/GitUITests/UITest.cs
@@ -1,38 +1,24 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CommonTestUtils;
 using GitUI;
+using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
 
 namespace GitUITests
 {
     public static class UITest
     {
-        public static async Task WaitForIdleAsync()
-        {
-            var idleCompletionSource = new TaskCompletionSource<VoidResult>();
-            Application.Idle += HandleApplicationIdle;
-
-            // Queue an event to make sure we don't stall if the application was already idle
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-            await Task.Yield();
-
-            await idleCompletionSource.Task;
-            Application.Idle -= HandleApplicationIdle;
-
-            void HandleApplicationIdle(object sender, EventArgs e)
-            {
-                idleCompletionSource.TrySetResult(default);
-            }
-        }
-
         public static void RunForm<T>(
             Action showForm,
-            Func<T, Task> runAsync)
+            Func<T, Task> runTestAsync)
             where T : Form
         {
+            var idleCompletionSource = new TaskCompletionSource<VoidResult>();
+
             // Start runAsync before calling showForm.
             // The latter might block until the form is closed, especially if using Application.Run(form).
 
@@ -42,18 +28,23 @@ namespace GitUITests
             var test = ThreadHelper.JoinableTaskContext.Factory.RunAsync(async () =>
             {
                 // Wait for the form to be opened by the test thread.
-                await WaitForIdleAsync();
+                await idleCompletionSource.Task;
                 var form = Application.OpenForms.OfType<T>().Single();
-                Assert.IsTrue(form.Visible);
-
-                // Wait for potential pending asynchronous tasks triggered by the form.
-                AsyncTestHelper.WaitForPendingOperations(AsyncTestHelper.UnexpectedTimeout);
-
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                if (!form.Visible)
+                {
+                    var errorMessage = $"{form.GetType().FullName} {form.Name} should be visible";
+                    form.Dispose();
+                    Assert.Fail(errorMessage);
+                }
 
                 try
                 {
-                    await runAsync(form);
+                    // Wait for potential pending asynchronous tasks triggered by the form.
+                    AsyncTestHelper.WaitForPendingOperations(AsyncTestHelper.UnexpectedTimeout);
+
+                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                    await runTestAsync(form);
                 }
                 finally
                 {
@@ -61,10 +52,45 @@ namespace GitUITests
                 }
             });
 
-            showForm();
+            // The following call to showForm will trigger messages.
+            // So we can be sure we don't stall waiting for idle if the application was already idle.
+            Application.Idle += HandleApplicationIdle;
+            try
+            {
+                try
+                {
+                    showForm();
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        // Wake up and join the asynchronous test operation.
+                        idleCompletionSource.TrySetResult(default);
+                        test.Join();
+                    }
+                    catch (Exception)
+                    {
+                        // ignore
+                    }
 
-            // Join the asynchronous test operation so any exceptions are rethrown on this thread
-            test.Join();
+                    throw;
+                }
+
+                // Join the asynchronous test operation so any exceptions are rethrown on this thread.
+                test.Join();
+            }
+            finally
+            {
+                Application.Idle -= HandleApplicationIdle;
+            }
+
+            return;
+
+            void HandleApplicationIdle(object sender, EventArgs e)
+            {
+                idleCompletionSource.TrySetResult(default);
+            }
         }
 
         private readonly struct VoidResult
