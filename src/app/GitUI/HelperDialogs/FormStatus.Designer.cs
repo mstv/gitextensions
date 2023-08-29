8c91e73cf0e0a29d244a3ec9d95aa28b4d189158
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2023-08-29 18:59:25 +0300

    Add password text box to remote console window

--- src/app/GitUI/HelperDialogs/FormStatus.Designer.cs
+++ src/app/GitUI/HelperDialogs/FormStatus.Designer.cs
@@ -18,10 +18,15 @@
             Ok = new Button();
             ProgressBar = new ProgressBar();
             KeepDialogOpen = new CheckBox();
+            ShowPassword = new CheckBox();
             Abort = new Button();
             pnlOutput = new Panel();
+            PasswordPanel = new Panel();
+            Password = new TextBox();
+            PasswordSend = new Button();
             MainPanel.SuspendLayout();
             ControlsPanel.SuspendLayout();
+            PasswordPanel.SuspendLayout();
             SuspendLayout();
             // 
             // MainPanel
@@ -35,7 +40,8 @@
             ControlsPanel.Controls.Add(Abort);
             ControlsPanel.Controls.Add(Ok);
             ControlsPanel.Controls.Add(KeepDialogOpen);
-            ControlsPanel.Location = new Point(0, 249);
+            ControlsPanel.Controls.Add(ShowPassword);
+            ControlsPanel.Location = new Point(0, 288);
             ControlsPanel.Size = new Size(549, 39);
             // 
             // Ok
@@ -70,11 +76,23 @@
             KeepDialogOpen.Location = new Point(254, 8);
             KeepDialogOpen.Name = "KeepDialogOpen";
             KeepDialogOpen.Size = new Size(120, 22);
-            KeepDialogOpen.TabIndex = 2;
-            KeepDialogOpen.Text = "Keep dialog open";
+            KeepDialogOpen.TabIndex = 3;
+            KeepDialogOpen.Text = "&Keep dialog open";
             KeepDialogOpen.UseCompatibleTextRendering = true;
             KeepDialogOpen.UseVisualStyleBackColor = true;
             KeepDialogOpen.CheckedChanged += KeepDialogOpen_CheckedChanged;
+            // 
+            // ShowPassword
+            // 
+            ShowPassword.AutoSize = true;
+            ShowPassword.Location = new Point(108, 8);
+            ShowPassword.Name = "ShowPassword";
+            ShowPassword.Size = new Size(140, 22);
+            ShowPassword.TabIndex = 2;
+            ShowPassword.Text = "Show &password input";
+            ShowPassword.UseCompatibleTextRendering = true;
+            ShowPassword.UseVisualStyleBackColor = true;
+            ShowPassword.CheckedChanged += ShowPassword_CheckedChanged;
             // 
             // Abort
             // 
@@ -100,14 +118,50 @@
             pnlOutput.Size = new Size(549, 246);
             pnlOutput.TabIndex = 0;
             // 
+            // PasswordPanel
+            // 
+            PasswordPanel.Controls.Add(Password);
+            PasswordPanel.Controls.Add(PasswordSend);
+            PasswordPanel.Dock = DockStyle.Bottom;
+            PasswordPanel.Location = new Point(0, 249);
+            PasswordPanel.Margin = new Padding(0);
+            PasswordPanel.Name = "PasswordPanel";
+            PasswordPanel.Padding = new Padding(8);
+            PasswordPanel.Size = new Size(549, 39);
+            PasswordPanel.TabIndex = 2;
+            // 
+            // Password
+            // 
+            Password.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            Password.Location = new Point(12, 8);
+            Password.Margin = new Padding(0);
+            Password.Name = "Password";
+            Password.Size = new Size(443, 23);
+            Password.TabIndex = 0;
+            Password.UseSystemPasswordChar = true;
+            // 
+            // PasswordSend
+            // 
+            PasswordSend.Anchor = AnchorStyles.Right;
+            PasswordSend.Location = new Point(461, 8);
+            PasswordSend.MinimumSize = new Size(75, 23);
+            PasswordSend.Name = "PasswordSend";
+            PasswordSend.Size = new Size(75, 23);
+            PasswordSend.TabIndex = 1;
+            PasswordSend.Text = "Send input";
+            PasswordSend.UseCompatibleTextRendering = true;
+            PasswordSend.UseVisualStyleBackColor = true;
+            PasswordSend.Click += PasswordSend_Click;
+            // 
             // FormStatus
             // 
             AcceptButton = Ok;
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
             CancelButton = Abort;
-            ClientSize = new Size(549, 288);
+            ClientSize = new Size(549, 327);
             Controls.Add(ProgressBar);
+            Controls.Add(PasswordPanel);
             MaximizeBox = false;
             MinimizeBox = false;
             MinimumSize = new Size(500, 200);
@@ -115,21 +169,27 @@
             StartPosition = FormStartPosition.CenterParent;
             Text = "Process";
             Controls.SetChildIndex(ControlsPanel, 0);
+            Controls.SetChildIndex(PasswordPanel, 0);
             Controls.SetChildIndex(ProgressBar, 0);
             Controls.SetChildIndex(MainPanel, 0);
             MainPanel.ResumeLayout(false);
             ControlsPanel.ResumeLayout(false);
             ControlsPanel.PerformLayout();
+            PasswordPanel.ResumeLayout(false);
+            PasswordPanel.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
-
         }
 
         #endregion
 
         private ProgressBar ProgressBar;
+        protected Panel PasswordPanel;
+        protected TextBox Password;
+        protected Button PasswordSend;
         protected Button Ok;
         protected CheckBox KeepDialogOpen;
+        protected CheckBox ShowPassword;
         protected Button Abort;
         protected Panel pnlOutput;
     }
