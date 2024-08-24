36a3e318b591e16bb06746679bb4b11dbc9d45a4
Michael Seibt <mstv@gmx.net>
2024-08-24 17:21:11 +0200

    feat(LinesMatcher): Then match by words

--- src/app/GitUI/Editor/Diff/LinesMatcher.cs
+++ src/app/GitUI/Editor/Diff/LinesMatcher.cs
@@ -73,7 +73,58 @@ internal static class LinesMatcher
             return (Array.IndexOf(removed, longestMatchingRemoved), matchingAddedIndex);
         }
 
-        return (0, 0);
+        int removedMaxScoreIndex = 0;
+        int addedMaxScoreIndex = 0;
+        float maxScore = -1;
+        foreach ((int removedIndex, int addedIndex) in GetAllCombinations(removed.Length, added.Length))
+        {
+            float score = GetWordMatchScore(removed[removedIndex], added[addedIndex]);
+            if (maxScore < score)
+            {
+                maxScore = score;
+                removedMaxScoreIndex = removedIndex;
+                addedMaxScoreIndex = addedIndex;
+                if (maxScore == 1)
+                {
+                    return (removedMaxScoreIndex, addedMaxScoreIndex);
+                }
+            }
+        }
+
+        return (removedMaxScoreIndex, addedMaxScoreIndex);
+
+        static float GetWordMatchScore(LineData r, LineData a)
+        {
+            if (r.Words.Count == 0 || a.Words.Count == 0)
+            {
+                return -1;
+            }
+
+            return (float)r.Words.Intersect(a.Words).Count() / Math.Max(r.Words.Count, a.Words.Count);
+        }
+    }
+
+    internal static IEnumerable<(int FirstIndex, int SecondIndex)> GetAllCombinations(int firstEnd, int secondEnd)
+    {
+        // upper left half including prinicipal diagonal
+        for (int diagonalIndex = 0; diagonalIndex < firstEnd; ++diagonalIndex)
+        {
+            int diagonalEnd = Math.Min(diagonalIndex + 1, secondEnd);
+            for (int secondIndex = 0; secondIndex < diagonalEnd; ++secondIndex)
+            {
+                yield return (FirstIndex: diagonalIndex - secondIndex, secondIndex);
+            }
+        }
+
+        // lower right half
+        for (int diagonalIndex = 1; diagonalIndex < secondEnd; ++diagonalIndex)
+        {
+            int diagonalEnd = Math.Min(firstEnd + diagonalIndex, secondEnd);
+            for (int secondIndex = diagonalIndex; secondIndex < diagonalEnd; ++secondIndex)
+            {
+                yield return (FirstIndex: firstEnd - 1 + diagonalIndex - secondIndex, secondIndex);
+            }
+        }
     }
 
     internal static IEnumerable<string> GetWords(string text, Func<char, bool> isWordChar)
---
+++ tests/app/UnitTests/GitUI.Tests/Editor/Diff/LinesMatcherTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using GitUI.Editor.Diff;
+using ICSharpCode.TextEditor.Document;
+
+namespace GitUITests.Editor.Diff;
+
+[TestFixture]
+public class LinesMatcherTests
+{
+    [Test]
+    public void GetAllCombinations([Range(1, 4)] int firstEnd, [Range(1, 4)] int secondEnd)
+    {
+        bool[,] visited = new bool[firstEnd, secondEnd];
+        foreach ((int firstIndex, int secondIndex) in LinesMatcher.GetAllCombinations(firstEnd, secondEnd))
+        {
+            firstIndex.Should().BeLessThan(firstEnd);
+            secondIndex.Should().BeLessThan(secondEnd);
+            visited[firstIndex, secondIndex].Should().BeFalse();
+            visited[firstIndex, secondIndex] = true;
+        }
+
+        for (int i = 0; i < firstEnd; ++i)
+        {
+            for (int j = 0; j < secondEnd; ++j)
+            {
+                visited[i, j].Should().BeTrue($"everywhere including at {i}, {j}");
+            }
+        }
+    }
+
+    [Test]
+    [TestCase("", new string[] { })]
+    [TestCase("a", new string[] { "a" })]
+    [TestCase("a-", new string[] { "a" })]
+    [TestCase("-a", new string[] { "a" })]
+    [TestCase("a bc", new string[] { "a", "bc" })]
+    [TestCase("-a bc", new string[] { "a", "bc" })]
+    [TestCase("a bc-", new string[] { "a", "bc" })]
+    [TestCase("---abc---123---def_7---", new string[] { "abc", "123", "def_7" })]
+    public void GetWords(string text, string[] words)
+    {
+        LinesMatcher.GetWords(text, TextUtilities.IsLetterDigitOrUnderscore)
+            .Should().BeEquivalentTo(words);
+    }
+}
