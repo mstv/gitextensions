20101cc58e50b827faec948c9bd897511a90d481
Michael Seibt <mstv@gmx.net>
2024-08-24 17:20:52 +0200

    feat(LinesMatcher): Detect indented lines

--- src/app/GitUI/Editor/Diff/LinesMatcher.cs
+++ src/app/GitUI/Editor/Diff/LinesMatcher.cs
@@ -64,6 +64,15 @@ internal static class LinesMatcher
 
     private static (int RemovedIndex, int AddedIndex) FindBestMatch(LineData[] removed, LineData[] added)
     {
+        // search longest match of Trimmed
+        (LineData longestMatchingRemoved, int matchingAddedIndex)
+            = removed.Select(r => (r, addedIndex: added.IndexOf(a => a.Trimmed == r.Trimmed)))
+                     .MaxBy(pair => pair.addedIndex < 0 ? -1 : pair.r.Trimmed.Length);
+        if (matchingAddedIndex >= 0)
+        {
+            return (Array.IndexOf(removed, longestMatchingRemoved), matchingAddedIndex);
+        }
+
         return (0, 0);
     }
 
