9840a6e9d1836b4caa446fb3570466af8a1f2f49
Michael Seibt <mstv@gmx.net>
2024-12-09 00:22:22 +0100

    Revert "fix(blame previous): Remove restriction"
    This reverts commit 07524d04764fc387de39b9e906d53dcd4423cc2f.

--- src/app/GitUI/UserControls/BlameControl.cs
+++ src/app/GitUI/UserControls/BlameControl.cs
@@ -569,7 +569,15 @@ namespace GitUI.Blame
             }
 
             blameRevisionToolStripMenuItem.Enabled = true;
-            blamePreviousRevisionToolStripMenuItem.Enabled = _revisionGridInfo?.GetRevision(blameinfo.selectedRevision.ObjectId)?.HasParent is true;
+            blamePreviousRevisionToolStripMenuItem.Enabled = RevisionHasParent(blameinfo.selectedRevision);
+
+            // Get parent for the actual revision, the selected revision may have rewritten parents.
+            // The menu will be slightly slower in this situation.
+            bool RevisionHasParent(GitRevision? selectedRevision)
+            {
+                GitRevision actualRevision = _revisionGridInfo?.GetActualRevision(selectedRevision);
+                return (actualRevision?.HasParent ?? false) && (_revisionGridInfo?.GetRevision(actualRevision?.FirstParentId) is not null);
+            }
         }
 
         private GitBlameCommit? GetBlameCommit()
@@ -644,12 +652,16 @@ namespace GitUI.Blame
                 return;
             }
 
+            // Try get actual parent revision, get popup if it does not exist.
+            // (The menu should be disabled if previous is not in grid).
+            GitRevision selectedRevision = _revisionGridInfo!.GetActualRevision(blameInfo.selectedRevision);
+
             // Origin line of commit selected is final line of the previous blame commit
             int finalLineNumberOfPreviousBlame = _lastBlameLine!.OriginLineNumber;
-            int originalLineNumberOfPreviousBlame = _gitBlameParser.GetOriginalLineInPreviousCommit(blameInfo.selectedRevision, blameInfo.filename, finalLineNumberOfPreviousBlame);
+            int originalLineNumberOfPreviousBlame = _gitBlameParser.GetOriginalLineInPreviousCommit(selectedRevision, blameInfo.filename, finalLineNumberOfPreviousBlame);
 
             _clickedBlameLine = new GitBlameLine(_lastBlameLine.Commit, finalLineNumberOfPreviousBlame, originalLineNumberOfPreviousBlame, "Dummy Git blame line used only to store the good 'originLineNumber' value to display and select it");
-            BlameRevision(blameInfo.selectedRevision.FirstParentId, blameInfo.filename);
+            BlameRevision(selectedRevision.FirstParentId, blameInfo.filename);
         }
 
         /// <summary>
