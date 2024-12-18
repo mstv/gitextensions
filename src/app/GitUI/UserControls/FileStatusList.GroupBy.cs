53de1e3558d5d426a9dde07f851bd6bc435a7362
Michael Seibt <mstv@gmx.net>
2024-12-19 00:12:30 +0100

    feat(FileStatusList): GroupBy

---
+++ src/app/GitUI/UserControls/FileStatusList.GroupBy.cs
@@ -0,0 +1,12 @@
+﻿using GitExtensions.Extensibility.Git;
+
+namespace GitUI;
+
+partial class FileStatusList
+{
+    private record GroupBy(
+        Func<GitItemStatus, string> GetGroupKey,
+        Func<string, string> GetImageKey,
+        Func<string, string> GetLabel,
+        bool Reverse = false);
+}
