5ff92f314bf3e167f21400dffce03dc565326fab
Michael Seibt <mstv@gmx.net>
2025-01-07 22:42:35 +0100

    feat(FileStatusList): Display file icons
    for git-grep and if grouped by file extension
    
    Add git-grep subfolder content on expand

--- src/app/GitUI/UserControls/MultiSelectTreeView.cs
+++ src/app/GitUI/UserControls/MultiSelectTreeView.cs
@@ -54,6 +54,11 @@ public class MultiSelectTreeView : NativeTreeView
 
                 if (value is not null)
                 {
+                    if (value.TreeView is null)
+                    {
+                        this.ExpandTopDownTo(value);
+                    }
+
                     if (!_mouseClickHandled)
                     {
                         value.EnsureVerticallyVisible();
