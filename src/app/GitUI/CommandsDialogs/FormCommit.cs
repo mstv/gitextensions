e8a4075aec59a20140d0b6f41ddb495c812f9468
Michael Seibt <mstv@gmx.net>
2026-03-11 22:10:20 +0100

    feat(FormCommit): Always stage single file

--- src/app/GitUI/CommandsDialogs/FormCommit.cs
+++ src/app/GitUI/CommandsDialogs/FormCommit.cs
@@ -1751,12 +1751,23 @@ public sealed partial class FormCommit : GitModuleForm
 
     private void StageClick(object sender, EventArgs e)
     {
-        if (_currentFilesList != Unstaged || Module.IsBareRepository())
+        if (Module.IsBareRepository())
         {
             return;
         }
 
-        Stage(Unstaged.SelectedItems.Items().Where(s => !s.IsAssumeUnchanged && !s.IsSkipWorktree).ToList());
+        if (_currentFilesList != Unstaged)
+        {
+            if (Unstaged.FocusedItem is FileStatusItem item)
+            {
+                Stage([item.Item]);
+                Unstaged.SelectFirstVisibleItem();
+            }
+
+            return;
+        }
+
+        Stage(Unstaged.SelectedItems.Items());
         if (Unstaged.IsEmpty)
         {
             Message.Focus();
@@ -1766,7 +1777,7 @@ public sealed partial class FormCommit : GitModuleForm
     private void Unstaged_DoubleClick(object sender, EventArgs e)
     {
         _currentFilesList = Unstaged;
-        Stage(Unstaged.SelectedItems.Items().ToList());
+        Stage(Unstaged.SelectedItems.Items());
         if (Unstaged.IsEmpty)
         {
             Message.Focus();
@@ -1775,7 +1786,7 @@ public sealed partial class FormCommit : GitModuleForm
 
     private void StageAllAccordingToFilter()
     {
-        Stage(Unstaged.GitItemFilteredStatuses.Where(s => !s.IsAssumeUnchanged && !s.IsSkipWorktree).ToList());
+        Stage(Unstaged.GitItemFilteredStatuses);
         Unstaged.SetFilter(string.Empty);
         if (Unstaged.IsEmpty)
         {
@@ -1843,8 +1854,9 @@ public sealed partial class FormCommit : GitModuleForm
         }
     }
 
-    private void Stage(IReadOnlyList<GitItemStatus> items)
+    private void Stage(IEnumerable<GitItemStatus> possibleItems)
     {
+        GitItemStatus[] items = [.. possibleItems.Where(s => !s.IsAssumeUnchanged && !s.IsSkipWorktree)];
         using (WaitCursorScope.Enter())
         {
             EnableStageButtons(false);
@@ -1854,7 +1866,7 @@ public sealed partial class FormCommit : GitModuleForm
 
                 Unstaged.StoreNextItemToSelect();
                 toolStripProgressBar1.Visible = true;
-                toolStripProgressBar1.Maximum = items.Count * 2;
+                toolStripProgressBar1.Maximum = items.Length * 2;
                 toolStripProgressBar1.Value = 0;
 
                 List<GitItemStatus> files = [];
